d44d9b0e4126d9fb1712fbf8bd6a61fc0aa64b5c
Milkias Tonji <37698254+MilkiasTonji@users.noreply.github.com>
2024-03-18 11:49:31 +0300

    ddrrr

--- src/utils/data.js
+++ src/utils/data.js
@@ -16,22 +16,22 @@ export const pages = [
       id: 4,
       name: "Page 4",
     },
-    {
-      id: 5,
-      name: "Page 5",
-    },
-    {
-      id: 6,
-      name: "Page 6",
-    },
-    {
-      id: 7,
-      name: "Page 7",
-    },
-    {
-      id: 8,
-      name: "Page 8",
-    },
+    // {
+    //   id: 5,
+    //   name: "Page 5",
+    // },
+    // {
+    //   id: 6,
+    //   name: "Page 6",
+    // },
+    // {
+    //   id: 7,
+    //   name: "Page 7",
+    // },
+    // {
+    //   id: 8,
+    //   name: "Page 8",
+    // },
   ];
 
   
